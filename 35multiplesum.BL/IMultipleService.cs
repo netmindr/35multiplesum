67dba2f7a4024d7be56d5f6a277241acbb768f21
netmindr <netmindr@comcast.net>
2019-05-07 00:41:08 -0500

    Initial Commit

---
+++ 35multiplesum.BL/IMultipleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _35multiplesum.BL
+{
+    public interface IMultipleService
+    {
+        Tuple<List<int>, int> GetSumOfMultiples(List<int> bases, int maxScope);
+    }
+}
---
+++ 35multiplesum.BL/MultipleService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _35multiplesum.BL
+{
+    public class MultipleService : IMultipleService
+    {
+        public Tuple<List<int>, int> GetSumOfMultiples(List<int> bases, int maxScope)
+        {
+            int sum = 0;
+            List<int> multiples = new List<int>();
+            for (int i = 1; i < maxScope; i++)
+            {
+                if (bases.Any(x => i%x == 0))
+                {
+                    multiples.Add(i);
+                    sum += i;
+                }
+                //sum += bases.FirstOrDefault(x => i % x == 0) == 0 ? i : 0;
+            }
+            return new Tuple<List<int>, int>(multiples, sum);
+        }
+    }
+}
